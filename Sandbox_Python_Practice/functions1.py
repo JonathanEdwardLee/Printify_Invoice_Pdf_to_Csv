e14a2caf4992b43d014e97f468a1865239f82169
Jonathan Edward Lee <94152189+JonathanEdwardLee@users.noreply.github.com>
2024-01-11 22:16:07 -0600

    practice with functions

---
+++ Sandbox_Python_Practice/functions1.py
@@ -0,0 +1,12 @@
+# writing functions
+# Jonathan Lee
+
+def add(x,y):
+    return x + y
+
+def multiply(x,y):
+    return x * y
+
+a = float(input("Please enter a number: "))
+b = float(input("Please enter a number: "))
+print ("Sum: ", add(a,b), "Product: ", multiply (a,b))
